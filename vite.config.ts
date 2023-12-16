79aa3994de0599aa12c96fa50234add2ee844a12
rustynail0105 <peterdekker.dev@gmail.com>
2023-12-16 21:08:46 +0900

    resolve buffer issue

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { resolve } from "path";
+import inject from '@rollup/plugin-inject';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,7 +13,13 @@ export default defineConfig({
       constants: resolve(__dirname, "./src/constants"),
     },
   },
-  plugins: [react()],
+  plugins: [
+    react(),
+    inject({
+      Buffer: ["buffer", "Buffer"],
+      process: "process",
+    }),
+  ],
   optimizeDeps: {
     include: ["**/*.svg"],
   },
