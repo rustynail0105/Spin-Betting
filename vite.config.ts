d8801256a4f4725c005b4b205fcb5f55c96b75d2
rustynail0105 <peterdekker.dev@gmail.com>
2023-12-16 21:16:44 +0900

    add buffer global variable

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { resolve } from "path";
-import inject from '@rollup/plugin-inject';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -13,13 +12,12 @@ export default defineConfig({
       constants: resolve(__dirname, "./src/constants"),
     },
   },
-  plugins: [
-    react(),
-    inject({
-      Buffer: ["buffer", "Buffer"],
-      process: "process",
-    }),
-  ],
+  define: {
+    "process.env": process.env,
+    global: {},
+    Buffer: ["buffer", "Buffer"],
+  },
+  plugins: [react()],
   optimizeDeps: {
     include: ["**/*.svg"],
   },
