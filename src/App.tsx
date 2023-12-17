041803b4d963dec06ff58d6fab879ea50df0b064
rustynail0105 <peterdekker.dev@gmail.com>
2023-12-17 20:48:25 +0900

    remove useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useMemo, useState } from "react";
+import { useMemo, useState } from "react";
 import { clusterApiUrl } from "@solana/web3.js";
 import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
 import {
